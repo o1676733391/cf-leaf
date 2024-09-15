480c2ba52b2e7c720a0d412316feaad39d4090f6
May Cloudy <o1676733391@gmail.com>
2024-09-15 07:47:09 +0700

    fix word check

--- Components/DownloadAllFiles.js
+++ Components/DownloadAllFiles.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import { storage } from '../firebaseConfig';
 import { ref, listAll, getDownloadURL } from "firebase/storage";
 import JSZip from 'jszip';
-import { saveAs } from 'file-saver';
+import { saveAs } from 'file-server';
 import styles from './DownloadAllFiles.module.css';
 
 const DownloadAllFiles = () => {
